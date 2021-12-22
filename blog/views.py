e2ac7e01570e9bed1defb5c5b74e96c19ca6238a
Shivam Sharma <shivam.usharma@gmail.com>
2021-12-22 11:52:32 +0530

    Admin Page, Database and Migrations, User Registration

--- blog/views.py
+++ blog/views.py
@@ -1,24 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Rahul Kohli',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'December 15, 2021'
-    },
-    {
-        'author': 'Virat KL',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'December 16, 2021'
-    }
-]
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
